c6ad76997cada7f1bf22968a23432439276703de
Charlie Lloyd <sccjl@leeds.ac.uk>
2017-03-28 14:40:38 +0100

    Convert code to functions and data frames

--- timeDependence/Code/rawDataProcessing.py
+++ timeDependence/Code/rawDataProcessing.py
@@ -2,7 +2,18 @@
 ##############		RAW DATA PROCESSING SCRIPT	###################################
 ####
 ####		This script processes raw data from the time-dependence tests of Mar2017.
-####		Data is read in, processed, and saved as separate data files for plotting.
+####		Data is currently processed using two functions:
+####
+####			txtToDataFrame:	This function reads in the txt files and returns a
+####					data frame consisting of probe position, sample
+####					number, time stamp and two velocity components.
+####
+####			rawToProcessed:	Adds additional columns onto the dataFrame such as
+####					means, RMS velocities and reynolds stresses.
+####
+####		Currently working on the plotting scripts. The modularity of the script
+####		will later allow the addition of filtering operations to the raw time 
+####		series.
 ####
 ###########################################################################################
 ####
@@ -10,94 +21,156 @@
 import numpy as np
 import re
 import matplotlib.pyplot as mpl
+import pandas as pd
 #
-##		Read in data (This will later be looped as a function or otherwise)
-#fileName = "../Data/rawData/4hz/Run8_x400_fl_4hz_300secs/Run8_x400_fl_4_hz_300secs.000003.txt"
-def txtToArray (fileName):
+###########################################################################################
+####		FUNCTION DEFINITON:	txtToDataFrame				###########
+####
+####		This function takes a given txt file, consisting of LDA data, and returns
+####		a dataFrame for further analysis. Data frame consists of several columns:
+####			column 1:	NXYZ	(file number and probe position)
+####			column 2:	sampleNumber
+####			column 3:	timeStamp
+####			column 4:	Ux
+####			column 5:	Uy
+####
+###########################################################################################
+def txtToDataFrame (fileName):
+#	Open the input file and store the line data as 'content'
 	with open(fileName) as f:
 		content=f.readlines()
 		content = [x.strip() for x in content]
-##		Content is a list of strings, separated by \n, containing each lines
-##		First few lines represent header
-##		Need to identify XYZ from header
-##		Need to identify row number containing first data point
+##	Content is a list of strings, separated by \n, containing each line
+##	First few lines represent header
 ##
-##		First grab the file number and position from the content list
+##	Identify probe position:
+##	Achieved by testing to see if the current line, when split by ';'
+##	returns a length of 4. This length corresponds to the correct line.
+#
 	i=0
-	while len(content[i].split(';')) is not 4:	#Test to see if the current line, when split by ';', has a length of 4
-		i=i+1					#A length of 4 will correspond to the line containing NXYZ
-	NXYZ = content[i].split(';')			#Stores the row containing the position and splits it by ';'
-	for i in range(len(NXYZ)):			#loop through the list NXYZ	
-		NXYZ[i] = re.sub(' mm$','',NXYZ[i])	#remove units and store only scalars
-#		NXYZ is a list containing only numbers, corresponding to file number and XYZ positions.
-##
-##		Now check to find the raw data
-##		Data column names are split by tabs and the first entry is always "Row#"
-##		We use another while loop to find this row number and then we add one to find the data
+	while len(content[i].split(';')) is not 4:	
+		i=i+1
+##	Store the row containing the position and splits it by ';'					
+	NXYZ = content[i].split(';')
+##	Clean up the list NXYZ and store only scalars
+	for i in range(len(NXYZ)):				
+		NXYZ[i] = re.sub(' mm$','',NXYZ[i])	
+##	Now check to find the raw data
+##	Data column names are split by tabs and the first entry is always "Row#"
+##	We use another while loop to find this row number and then we add one to find the data
 	i=0
 	for i in range(len(content)):
 		if re.match(r'"Row#"',content[i].split('\t')[0]):
 			index=i+1
 			break
-##		Now we have located this start of the data by the index variable
-##		Now loop through the length of content and extract the important variables
-##
-##		DATA ARRANGED AS:	sampleNumber,	timeStamp,	timeInterval,	Ux,	Uy
-##		We don't need to extract timeInterval but the others are useful.
-##
-#print(content[index])
+##	Now we have located this start of the data by the index variable
+##	Now loop through the length of content and extract variables
+##	Extract sample number, time stamp and two components of velocity
+#
+#	txt file arranged as:
+#			column 0:	sampleNumber
+#			column 1:	timeStamp
+#			column 2:	timeInterval (ignore this)
+#			column 3:	Ux
+#			column 4:	Uy
+#
 	sampleNumber,timeStamp,Ux,Uy = [],[],[],[]
 	for i in range(index,len(content)):
 		sampleNumber.append(content[i].split('\t')[0])
 		timeStamp.append(content[i].split('\t')[1])
 		Ux.append(content[i].split('\t')[3])
 		Uy.append(content[i].split('\t')[4])
+#
+#	Store variables as lists of floats
 	sampleNumber=[float(i) for i in sampleNumber]
 	timeStamp=[float(i) for i in timeStamp]
-	timeStamp = np.divide(timeStamp,1000)		#change units to seconds
+#	change units to seconds
+	timeStamp = np.divide(timeStamp,1000)
 	Ux=[float(i) for i in Ux]
 	Uy=[float(i) for i in Uy]
-	dataStack = np.vstack([sampleNumber,timeStamp,Ux,Uy])
-	return	dataStack;
+#
+#	Change data to a dataFrame:
+#	First change each list to a series, then combine the series.
+#	Output the final data frame
+	NXYZ = pd.Series(NXYZ)
+	sampleNumber = pd.Series(sampleNumber)
+	timeStamp = pd.Series(timeStamp)
+	Ux = pd.Series(Ux)
+	Uy = pd.Series(Uy)
+	data = pd.DataFrame({'NXYZ':NXYZ,'sampleNumber':sampleNumber,'timeStamp':timeStamp,'Ux':Ux,'Uy':Uy})
+	return data;
 
-
-########################################################################################
+###########################################################################################
+####	Function Definition:		rawToProcessed				###########
 ####
-####	We now have 4 vectors containing raw data - now we create new variables
-##
-##	To compute means and stresses etc. we loop through the number of points and
-##	compute quantities based on the previous entries i.e check convergence as we 
-##	include more points / timeStamps.
+####	This function calculates statistical quantities from the data frame input (as 
+####	described above). The statistical quantities are added as additional columns to the
+####	original data frame.
+####
+####	Statistical quantities are calculated for each data entry using the previous values
+####	i.e this is used to check convergence of stats.
+####
+####		Current stats: UxMean, UyMean, uxRMS, uyRMS, uv
+####
+###########################################################################################
+def rawToProcessed (data):
+#
+##	Read in data frame and convert required variables to lists: This speeds up looping
+##	process.
+	Ux = data.Ux.tolist()
+	Uy = data.Uy.tolist()
+#
+##	Initialise statistics and loop through the length of the time series
+	UxMean,UyMean,uxRMS,uyRMS,uv = [],[],[],[],[]
+	for N in range(len(Ux)):
+		print(N, 'of', len(Ux))
+#
+##		initialise new values of means and calculate them based on previous N
+##		append the UxMean values with updated values
+		UxMeanNew, UyMeanNew = [],[]
+		UxMeanNew = np.divide(sum(Ux[0:N+1]),(N+1))
+		UxMean.append(UxMeanNew)
+		UyMeanNew = np.divide(sum(Uy[0:N+1]),(N+1))
+		UyMean.append(UyMeanNew)
+#
+##		calculate RMS velocities and Reynolds stresses
+		uxRMS.append(np.divide(sum(np.power(Ux-UxMeanNew,2)),N+1))
+		uyRMS.append(np.divide(sum(np.power(Uy-UyMeanNew,2)),N+1))
+		uv.append(np.divide(sum((Ux-UxMeanNew)*(Uy-UyMeanNew)),N+1))
+#
+##	Print output of selected stats against global values using numpy library
+##	If these quantities do not match check the code!
+	print(np.mean(Ux))
+	print(UxMean[-1])
+	print(np.sqrt(uxRMS[-1]))
+	print(np.std(Ux))
+#
+##	Add variables to existing data frame.
+##	Variables first need to be converted to 'pandas.series'
+	UxMean = pd.Series(UxMean)
+	UyMean = pd.Series(UyMean)
+	uxRMS = pd.Series(uxRMS)
+	uyRMS = pd.Series(uyRMS)
+	uv = pd.Series(uv)
+	data['UxMean']= UxMean
+	data['UyMean']= UyMean
+	data['uxRMS']= uxRMS
+	data['uyRMS']= uyRMS
+	data['uv']= uv
+	return data;
+
+###########################################################################################
 
 fileName = "../Data/rawData/8hz/Run9_x400_fl_8hz_300secs/Run9_x400_fl_8_hz_300secs.000001.txt"
-dataStack = txtToArray(fileName)
-sampleNumber=np.ndarray.tolist(dataStack[0])
-timeStamp=np.ndarray.tolist(dataStack[1])
-Ux=np.ndarray.tolist(dataStack[2])
-Uy=np.ndarray.tolist(dataStack[3])
+data = txtToDataFrame(fileName)
+data = rawToProcessed(data)
 
-UxMean,UyMean,uxRMS,uyRMS,uv = [],[],[],[],[]
-for N in range(0,len(sampleNumber)):
-#	Compute means
-#	
-#	print N, 'of', len(sampleNumber)
-	UxMeanNew = []
-	UyMeanNew = []
-	UxMeanNew = np.divide(sum(Ux[0:N+1]),(N+1))
-	UxMean.append(UxMeanNew)
-	UyMeanNew = np.divide(sum(Uy[0:N+1]),(N+1))
-	UyMean.append(UyMeanNew)
-#
-	uxRMS.append(np.divide(sum(np.power(Ux-UxMeanNew,2)),N+1))
-
-print np.sqrt(uxRMS[len(uxRMS)-1])
-print np.std(Ux)
 
 #	Add some lines at +/- 5%
-plus5=[np.mean(Ux)*0.05+np.mean(Ux)]*len(Ux)
-min5 = [np.mean(Ux)*-0.05+np.mean(Ux)]*len(Ux)
-plus1=[np.mean(Ux)*0.01+np.mean(Ux)]*len(Ux)
-min1 = [np.mean(Ux)*-0.01+np.mean(Ux)]*len(Ux)
+#plus5=[np.mean(Ux)*0.05+np.mean(Ux)]*len(Ux)
+#min5 = [np.mean(Ux)*-0.05+np.mean(Ux)]*len(Ux)
+#plus1=[np.mean(Ux)*0.01+np.mean(Ux)]*len(Ux)
+#min1 = [np.mean(Ux)*-0.01+np.mean(Ux)]*len(Ux)
 #mpl.plot(timeStamp,(np.divide(UxMean-np.mean(Ux),np.mean(Ux))))
 #mpl.plot(timeStamp,uxRMS)
 #mpl.plot(timeStamp,plus5)
